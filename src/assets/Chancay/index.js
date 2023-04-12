ec405eba7d444785f6aceace1935df9a55cf0c93
Kishinkidu <erick.revoredo58@gmail.com>
2023-04-12 17:24:05 -0500

    img y video

---
+++ src/assets/Chancay/index.js
@@ -0,0 +1,78 @@
+const listas = [
+    {
+        establecimiento: "discoteca",
+        distrito: "chancay",
+        nombre: "Kaema club",
+        direccion: "C. Lopez de Zuñiga, Chancay 15131 ",
+        logo: "./images/logo-miTercerLugar.jpg",
+        carta: "",//si es que hay
+        telefono: " 981 522 697"
+        horario: {
+            lun: "",
+            mar: "",
+            mie: "",
+            jue: "",
+            vie: "",
+            sab: "7:00pm a 4:00am",
+            dom: "7:00pm a 4:00am"
+        }
+
+    },
+    {
+        establecimiento: "bar",
+        distrito: "chancay",
+        nombre: "WAYKI'S BAR",
+        direccion: "Calle San Martin 153, Chancay 15131 ",
+        logo: "./images/logo-miTercerLugar.jpg",
+        carta: "",//si es que hay
+        telefono: "933 694 706"
+        horario: {
+            lun: "7:00pm a 2:00am",
+            mar: "7:00pm a 2:00am",
+            mie: "7:00pm a 2:00am",
+            jue: "7:00pm a 2:00am",
+            vie: "7:00pm a 2:00am",
+            sab: "7:00pm a 4:00am",
+            dom: "7:00pm a 4:00am"
+
+        }
+    },
+    {
+        establecimiento: "bar",
+        distrito: "chancay",
+        nombre: "la huerta encantada",
+        direccion: "Boulevard Diego, C. Lopez de Zuñiga 450, Chancay 15131",
+        logo: "./images/logo-miTercerLugar.jpg",
+        carta: "",//si es que hay
+        telefono: "946 519 352"
+        horario: {
+            lun: "12:00pm a 18:00pm",
+            mar: "12:00pm a 18:00pm",
+            mie: "12:00pm a 18:00pm",
+            jue: "12:00pm a 18:00pm",
+            vie: "12:00pm a 23:00pm",
+            sab: "12:00pm a 23:00pm",
+            dom: "12:00pm a 23:00pm"
+        }
+
+    },
+    {
+        establecimiento: "bar",
+        distrito: "chancay",
+        nombre: "Lobitos Restobar",
+        direccion: "Junin 380, Chancay 15131",
+        logo: "./images/logo-miTercerLugar.jpg",
+        carta: "cdn.me-qr.com",//si es que hay
+        telefono: "958 534 920"
+        horario: {
+            lun: "12:00pm a 18:00pm",
+            mar: "12:00pm a 18:00pm",
+            mie: "12:00pm a 18:00pm",
+            jue: "12:00pm a 18:00pm",
+            vie: "12:00pm a 23:00pm",
+            sab: "12:00pm a 23:00pm",
+            dom: "12:00pm a 23:00pm"
+        }
+
+    }
+]
---
+++ src/assets/Huaral/index.js
@@ -0,0 +1,59 @@
+const listas = [
+    {
+        establecimiento: "discoteca",
+        distrito: "huaral",
+        nombre: "Legon",
+        direccion: "Los Geranios 347, Huaral 15202 ",
+        logo: "",
+        carta: "",//si es que hay
+        telefono: " 934 557 270"
+        horario: {
+            lun: "",
+            mar: "",
+            mie: "",
+            jue: "",
+            vie: "",
+            sab: "7:00pm a 4:00am",
+            dom: "7:00pm a 4:00am"
+        }
+
+    },
+    {
+        establecimiento: "discoteca",
+        distrito: "huaral",
+        nombre: "Gothik Disco",
+        direccion: "Julio C. Tello 152, Huaral 15202 ",
+        logo: "",
+        carta: "",//si es que hay
+        telefono: ""
+        horario: {
+            lun: "",
+            mar: "",
+            mie: "",
+            jue: "",
+            vie: "",
+            sab: "22:00pm a 6:00am",
+            dom: "22:00pm a 6:00am"
+        }
+
+    },
+    {
+        establecimiento: "discoteca",
+        distrito: "huaral",
+        nombre: "casa vieja",
+        direccion: "Perú 120, Huaral 15202 ",
+        logo: "",
+        carta: "",//si es que hay
+        telefono: " "
+        horario: {
+            lun: "",
+            mar: "",
+            mie: "",
+            jue: "",
+            vie: "",
+            sab: "23:00pm a 6:00am",
+            dom: ""
+        }
+
+    },
+    ]
