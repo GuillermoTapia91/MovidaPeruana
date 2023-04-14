85d697047b9bafea31f61c79cfc03f433de31e2e
Kishinkidu <erick.revoredo58@gmail.com>
2023-04-14 17:05:29 -0500

    portada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,47 +5,49 @@ import './indexJercy.css'
 //Guillermo
 import discoImg from "../../assets/img/buscarPorDistrito_disco.jpg"
 import barImg from "../../assets/img/buscarPorDistrito_bar.jpg"
-import { useState,useRef } from "react"
+import { useState, useRef } from "react"
 import logo1 from "../../assets/Sanmiguel/MiTercerLugar/logo.jpg"
 import logo2 from "../../assets/Sanmiguel/PachaInti/logo.jpg"
 import logo3 from "../../assets/Sanmiguel/EnLounge&Bar/logo.jpg"
 import logo4 from "../../assets/Sanmiguel/FestivalSanMiguel/logo.jpg"
 import logo11 from "../../assets/Miraflores/wahios/logo.jpg"
-
+//Erick
+import video from "../../assets/video/discoteca.mp4"
+import Buble from "../../assets/img/burbuja7 (1).png"
 
 export default function Home() {
   //Guillermo
   let refMenuBtn = useRef()
   let refDiv1 = useRef()
   let refDiv2 = useRef()
-  const [buttonBuscar,setButtonBuscar]= useState(false)
-  const [selectDistritos,setSelectDistritos]= useState("")
-  
+  const [buttonBuscar, setButtonBuscar] = useState(false)
+  const [selectDistritos, setSelectDistritos] = useState("")
+
   const handleButton = (e) => {
-    if(refMenuBtn.current.textContent==="Buscar"){
+    if (refMenuBtn.current.textContent === "Buscar") {
       setButtonBuscar(true)
       refMenuBtn.current.textContent = "Cerrar"
       refDiv1.current.style.height = "auto"
       refDiv2.current.style.display = "block"
-    }else{
+    } else {
       setButtonBuscar(false)
       refMenuBtn.current.textContent = "Buscar"
       refDiv1.current.style.height = "100vh"
       refDiv2.current.style.display = "none"
     }
-    
+
   }
 
   const handleSelect = (e) => {
-    
+
     setSelectDistritos(e.target.value)
     console.log(selectDistritos)
   }
-  
+
   const [listas, setlistas] = useState([
     {
       establecimiento: "bar",
-      id:1,
+      id: 1,
       distrito: "san miguel",
       nombre: "Mi tercer lugar",
       direccion: "Av. La Mar 2261, San Miguel ",
@@ -62,11 +64,11 @@ export default function Home() {
       },
       fotos: [],
       web: "https://www.mitercerlugar.com.pe/"
-      
+
     },
     {
       establecimiento: "bar",
-      id:2,
+      id: 2,
       distrito: "san miguel",
       nombre: "EN lounge & bar",
       direccion: "Av. Riva Agüero 754, San Miguel",
@@ -86,7 +88,7 @@ export default function Home() {
     },
     {
       establecimiento: "bar",
-      id:3,
+      id: 3,
       distrito: "san miguel",
       nombre: "Festival San Miguel",
       direccion: "Av. la Marina 2517, San Miguel",
@@ -106,7 +108,7 @@ export default function Home() {
     },
     {
       establecimiento: "bar",
-      id:4,
+      id: 4,
       distrito: "san miguel",
       nombre: "Pacha Inti",
       direccion: "Mercedes Gallagher de Parks 396, San Miguel",
@@ -127,7 +129,7 @@ export default function Home() {
     {
       establecimiento: "discoteca",
       distrito: "san miguel",
-      id:5,
+      id: 5,
       nombre: "Sopranos",
       direccion: "Av. La Marina 3151,San Miguel",
       logo: "",
@@ -147,7 +149,7 @@ export default function Home() {
     {
       establecimiento: "bar",
       distrito: "san miguel",
-      id:6,
+      id: 6,
       nombre: "Sopranos",
       direccion: "Av. La Marina 1687,San Miguel",
       logo: "",
@@ -167,7 +169,7 @@ export default function Home() {
     {
       establecimiento: "bar",
       distrito: "san miguel",
-      id:7,
+      id: 7,
       nombre: "Rustica",
       direccion: "Av. La Marina 2631, San Miguel",
       logo: "../../assets/Sanmiguel/FestivalSanMiguel/logo.jpg",
@@ -187,7 +189,7 @@ export default function Home() {
     {
       establecimiento: "bar",
       distrito: "san miguel",
-      id:8,
+      id: 8,
       nombre: "Fridays",
       direccion: "Av. La Marina 2355 - C.C. La Marina Open Plaza,San Miguel",
       logo: "",
@@ -207,7 +209,7 @@ export default function Home() {
     {
       establecimiento: "bar",
       distrito: "san miguel",
-      id:9,
+      id: 9,
       nombre: "Fridays",
       direccion: "Av. La Marina 2000, C.C Plaza San Miguel",
       logo: "",
@@ -227,7 +229,7 @@ export default function Home() {
     {
       establecimiento: "bar",
       distrito: "san miguel",
-      id:10,
+      id: 10,
       nombre: "Chilis",
       direccion: "Calle Mantaro Nro. 220, C.C. Plaza San Miguel",
       logo: "",
@@ -247,7 +249,7 @@ export default function Home() {
     {
       establecimiento: "bar",
       distrito: "miraflores",
-      id:11,
+      id: 11,
       nombre: "Wahios",
       direccion: "C. Berlín 192, Miraflores",
       logo: logo11,
@@ -265,97 +267,109 @@ export default function Home() {
       web: "https://www.instagram.com/wahiosculturebar/?hl=es"
     },
   ])
-  return(
+  return (
     <div>
-      <div>
-        Erick 1
-      </div>
       <div >
-        Erick 2
+        {/*Erick*/}
+      <div className="container">
+                <video src={video} muted autoPlay loop>
+                </video>
+                <div className="capa"></div>
+                <div className="intro">
+                    <h1>Que comienze la Fiesta</h1>
+                    <h3>Encuentra la juerga mas cerca de tu zona</h3>
+                    <button className="busqueda btn btn-outline-primary">Bares/Discotecas</button>
+                </div>
+                <div>
+                    {/* <img className="base" src={Buble} alt="" /> */}
+                </div>
+            </div>
       </div>
+          {/* //Guillermo */}
+
       <div className="SectionBuscarPorDistritos  d-sm-flex justify-content-sm-center align-items-sm-center 
        gap-4 bg-black bg-gradient text-white p-2" ref={refDiv1}>
-          <div className="gap-1 mb-3">
+        <div className="gap-1 mb-3">
+          <div>
+            <h3 className="text-center tracking-in-contract-bck-bottom">Descubre bares y discotecas </h3>
+            <h3 className="text-center tracking-in-contract-bck-bottom"> disponibles en tu distrito</h3>
+          </div>
+          <div className=" input-container mt-3 d-flex   justify-content-center align-items-center gap-3 ">
+            <div className="inputg" >
+              {/* <input className="form-control" type="text" placeholder="Ej: San Miguel" id="input-buscar" /> */}
+              <select name="distrito" className="form-select" id="" onChange={handleSelect}>
+                <option value="">Distritos</option>
+                <option value="san miguel">San Miguel</option>
+                <option value="miraflores">Miraflores</option>
+                <option value="barranco">Barranco</option>
+                <option value="pueblo libre">Pueblo Libre</option>
+                <option value="los olivos">Los Olivos</option>
+                <option value="san juan de miraflores">San Juan de Miraflores</option>
+                <option value="chancay">Chancay</option>
+                <option value="huaral">Huaral</option>
+              </select>
+            </div>
             <div>
-              <h3 className="text-center tracking-in-contract-bck-bottom">Descubre bares y discotecas </h3>
-              <h3 className="text-center tracking-in-contract-bck-bottom"> disponibles en tu distrito</h3>
-            </div>
-            <div className=" input-container mt-3 d-flex   justify-content-center align-items-center gap-3 ">
-              <div className="inputg" >
-                {/* <input className="form-control" type="text" placeholder="Ej: San Miguel" id="input-buscar" /> */}
-                <select name="distrito"className="form-select" id="" onChange={handleSelect}>
-                  <option value="">Distritos</option>
-                  <option value="san miguel">San Miguel</option>
-                  <option value="miraflores">Miraflores</option>
-                  <option value="barranco">Barranco</option>
-                  <option value="pueblo libre">Pueblo Libre</option>
-                  <option value="los olivos">Los Olivos</option>
-                  <option value="san juan de miraflores">San Juan de Miraflores</option>
-                  <option value="chancay">Chancay</option>
-                  <option value="huaral">Huaral</option>
-                </select>
-              </div>
-              <div>
-                <button className="btn btn-color" ref={refMenuBtn} onClick={handleButton}>Buscar</button>
-              </div>
+              <button className="btn btn-color" ref={refMenuBtn} onClick={handleButton}>Buscar</button>
             </div>
           </div>
-          <div className=" d-sm-flex justify-content-sm-center align-items-sm-center gap-5 mt-3 text-center " >
-            <div className="mb-3"><img src={discoImg} width="380px" alt="" className="img-fluid heartbeat" /></div>
-            <div className=""><img src={barImg} width="400px" alt="" className="img-fluid heartbeat" /></div>
-          </div>
-      </div>          
+        </div>
+        <div className=" d-sm-flex justify-content-sm-center align-items-sm-center gap-5 mt-3 text-center " >
+          <div className="mb-3"><img src={discoImg} width="380px" alt="" className="img-fluid heartbeat" /></div>
+          <div className=""><img src={barImg} width="400px" alt="" className="img-fluid heartbeat" /></div>
+        </div>
+      </div>
       <div className="SectionBuscarFiltros bg-black bg-gradient p-3" ref={refDiv2} >
         <div className="container">
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">  
-            {listas.filter((listas)=>listas.distrito===selectDistritos).map((lista)=>
-            // <div className='mm' key={lista.id}>{lista.nombre}</div>
-            <div key={lista.id} className="col">
-            <div className="card">
-              <div className="card-body">
-                <div className="text-center">
-                  <img src={lista.logo} className="img-thumbnail" width="200px" alt="..."/>
-                </div>
-                <p className="mb-1 mt-1">Nombre: {lista.nombre}</p>
-                <p>Dirección: {lista.direccion}</p>
-                
-                <div className="text-center"  > 
-                  <button type="button" className="btn btn-color2" data-bs-toggle="modal" data-bs-target="#exampleModal">Horario</button>
-                
-                  <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby={lista.nombre}     aria-hidden="true">
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h1 className="modal-title fs-5" id="exampleModalLabel">Horario</h1>
-                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                          Horario:Lunes:{lista.horario.lun}
-                          Martes:{lista.horario.mar} 
-                          Miércoles:{lista. horario.mie}
-                          Jueves:{lista.horario.jue}
-                          Viernes:{lista.horario.vie}
-                          Sábado:${lista.horario.sab}
-                          Domingo:${lista.horario.dom}
-                        </div>
-                        <div className="modal-footer">
-                          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
+            {listas.filter((listas) => listas.distrito === selectDistritos).map((lista) =>
+              // <div className='mm' key={lista.id}>{lista.nombre}</div>
+              <div key={lista.id} className="col">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="text-center">
+                      <img src={lista.logo} className="img-thumbnail" width="200px" alt="..." />
+                    </div>
+                    <p className="mb-1 mt-1">Nombre: {lista.nombre}</p>
+                    <p>Dirección: {lista.direccion}</p>
+
+                    <div className="text-center"  >
+                      <button type="button" className="btn btn-color2" data-bs-toggle="modal" data-bs-target="#exampleModal">Horario</button>
+
+                      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby={lista.nombre} aria-hidden="true">
+                        <div className="modal-dialog">
+                          <div className="modal-content">
+                            <div className="modal-header">
+                              <h1 className="modal-title fs-5" id="exampleModalLabel">Horario</h1>
+                              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                              Horario:Lunes:{lista.horario.lun}
+                              Martes:{lista.horario.mar}
+                              Miércoles:{lista.horario.mie}
+                              Jueves:{lista.horario.jue}
+                              Viernes:{lista.horario.vie}
+                              Sábado:${lista.horario.sab}
+                              Domingo:${lista.horario.dom}
+                            </div>
+                            <div className="modal-footer">
+                              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                          </div>
                         </div>
                       </div>
+                      <button className="btn btn-color2 m-1">Fotos</button>
+                      <button className="btn btn-color2">Opiniones</button>
                     </div>
                   </div>
-                  <button className="btn btn-color2 m-1">Fotos</button>
-                  <button className="btn btn-color2">Opiniones</button>
-                </div>   
+                </div>
               </div>
-            </div>
-          </div>
             )}
             {/* arrays.filter(array=>array.algo === algo).map((array) =>()) */}
 
           </div>
         </div>
-      </div>  
+      </div>
       <div>
         Abraham1
       </div>
