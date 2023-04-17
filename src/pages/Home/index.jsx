a63e65dcd30b5beab69efd9d361a75cc5abdd1c7
Kishinkidu <erick.revoredo58@gmail.com>
2023-04-17 11:16:06 -0500

    video y capa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -297,7 +297,7 @@ export default function Home() {
         <div className="container">
           <video src={video} muted autoPlay loop>
           </video>
-          {/* <div className="capa"></div> */}
+          <div className="capa"></div>
           <div className="intro">
             <h1>Que comienze la Fiesta</h1>
             <h3>Encuentra la juerga mas cerca de tu zona</h3>
