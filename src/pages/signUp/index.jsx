2cdece812283ffa39f3d19c3a93f204fe37186ec
Erick Revoredo <erick.revoredo58@gmail.com>
2023-04-23 09:32:30 -0500

    singup

--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -4,25 +4,28 @@ import GitHublogo from "../../assets/img/github.svg"
 import GoogleLogo from "../../assets/img/GoogleLogo.png"
 import FacebookLogo from "../../assets/img/FacebookLogo.png"
 import TwitterLogo from "../../assets/img/TwitterLogo.png"
-export default function SignIn() {
-  return (
-    <div className="login-body">
+
+
+export default function SingUp (){
+    return(
+        <div className="login-body">
 
       <div className="container-sign">
         <img className="loginLogo" src={logo} alt="" />
         <h5 className="in-up">
-          <a className="inicio-crear mar-20" href="">Iniciar Sesión</a>
-          <a className="inicio-crear mar-20"href="http://localhost:3000/signup">Crear Cuenta</a>
+          <a className="inicio-crear mar-20" href="http://localhost:3000/signin">Iniciar Sesión</a>
+          <a className="inicio-crear mar-20"href="">Crear Cuenta</a>
         </h5 >
         {/* Iniciar Seccion */}
         <div className="login">
           <div className="bodyForm">
             <form className="formLogin" action="">
-              <h5 className="l" >Inicia sesión en tu cuenta de MovidaPeruana</h5>
+              <h5 className="l" >Llene el registro!</h5>
               <input className="W-80" type="email" placeholder="Correo Electronico"/>
               <input className="W-80" type="password" placeholder="Contraseña" />
-              <a className="inicio-crear" href="">Problemas para entrar?</a>
-              <button className="btn-signin" >SignIn</button>
+              <h5 className="l" >Introduce tu fecha de nacimiento</h5>
+              <input className="W-80" type="date" placeholder=""/>
+              <button className="btn-signin" >Registrarse</button>
             </form>
           </div>
 
@@ -55,5 +58,5 @@ export default function SignIn() {
         </div>
       </div>
     </div>
-  )
+    )
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomeView, SignInView } from "../pages";
+import { HomeView, SignInView, SignUpView } from "../pages";
 import MainLayout from "../Layouts/mainLayout";
+
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
           <Route path="/" element={<HomeView />} />
         </Route>
         <Route path="/SignIn" element={<SignInView />} />
+        <Route path="/SignUp" element={<SignUpView />} />
       </Routes>
     </BrowserRouter>
   );
