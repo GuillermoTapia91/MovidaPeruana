b0539f20807f3f9b542e79258627b10c50e2b3cf
Kishinkidu <erick.revoredo58@gmail.com>
2023-05-05 21:33:38 -0500

    account layout

--- src/service/Account/index.jsx
+++ src/service/Account/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../../supabase'
+import {footer} from "../../components/Footer/Footer"
+import "./style.css"
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true)
@@ -55,25 +57,78 @@ export default function Account({ session }) {
   }
 
   return (
-    <form onSubmit={updateProfile} className="form-widget d-flex flex-column align-items-center text-warning gap-3">
-      <div>
-        <h1>Perfil</h1>
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="username">Name</label>
-        <input
-          id="username"
-          type="text"
-          required
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-        {/* <div>
+    <div  className='tarjeta'>
+    <form onSubmit={updateProfile} className="form-widget d-flex flex-column align-items-center text-warning gap-3 index">
+      
+        <div>
+          <h1> Mi Perfil</h1>
+        </div>
+        <div className='d-flex gap-3'>
+          <label htmlFor="email">Correo</label>
+          <input id="email" type="text" value={session.user.email} disabled />
+        </div>
+        <div className='d-flex gap-3' >
+          <label htmlFor="username">Nombre de usuario</label>
+          <input
+            id="username"
+            type="text"
+            required
+            value={username || ''}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className='d-flex gap-3'>
+          <label htmlFor="Departamento">Departamento</label>
+          <select name="" id="Departamento">
+            <option value="Ancash">Ancash</option>
+            <option value="Ayacucho">Ayacucho</option>
+            <option value="Arequipa">Arequipa</option>
+            <option value="Amazonas">Amazonas</option>
+            <option value="Apurímac">Apurímac</option>
+            <option value="Cajamarca">Cajamarca</option>
+            <option value="Callao">Callao</option>
+            <option value="Cerro de Pasco">Cerro de Pasco</option>
+            <option value="Cusco">Cusco</option>
+            <option value="Huánuco">Huánuco</option>
+            <option value="Huancavelica">Huancavelica</option>
+            <option value="Ica"> Ica</option>
+            <option value="Junín">Junín</option>
+            <option value="Lima" selected>Lima</option>
+            <option value="La Libertad">La Libertad</option>
+            <option value="Lambayeque">Lambayeque</option>
+            <option value="Loreto">Loreto</option>
+            <option value="Madre de Dios">Madre de Dios</option>
+            <option value="Moquegua">Moquegua</option>
+            <option value="Piura">Piura</option>
+            <option value="Puno">Puno</option>
+            <option value="San Martín">San Martín</option>
+            <option value="Tacna">Tacna</option>
+            <option value="Tumbes">Tumbes</option>
+            <option value="Ucayali">Ucayali</option>
+          </select>
+        </div>
+        <div>
+          <button className="button block primary" type="submit" disabled={loading}>
+            {loading ? 'Loading ...' : 'Update'}
+          </button>
+        </div>
+
+        <div>
+          <button className="button block" type="button" onClick={() => supabase.auth.signOut()}>
+            Sign Out
+          </button>
+        </div>
+      
+    </form>
+
+    </div>
+    
+  )
+}
+
+
+
+{/* <div>
             <label htmlFor="website">Website</label>
             <input
             id="website"
@@ -82,18 +137,3 @@ export default function Account({ session }) {
             onChange={(e) => setWebsite(e.target.value)}
             />
         </div> */}
-
-      <div>
-        <button className="button block primary" type="submit" disabled={loading}>
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
-      <div>
-        <button className="button block" type="button" onClick={() => supabase.auth.signOut()}>
-          Sign Out
-        </button>
-      </div>
-    </form>
-  )
-}
