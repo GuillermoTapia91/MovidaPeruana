40e5ba838357fa0021b54f5cd3dd37f28df726ea
GuillermoTapia91 <guillermo.tapia1991@gmail.com>
2023-08-02 13:57:30 -0500

    se agrega page mi Información

---
+++ src/components/Table/table.jsx
@@ -0,0 +1,33 @@
+export default function Table(props) {
+  const { columns, rows } = props;
+
+  return (
+    <div className="mt-5 table-responsive">
+      <table className="table table-success table-striped">
+        <thead className="table-dark text-center">
+          <tr>
+            {columns.map((column) => (
+              <th scope="col" key={column.key}>
+                {column.text}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.length > 0 &&
+            rows.map((row) => (
+              <tr key={row.id} className="text-center">
+                {columns.map((column) => (
+                  <td key={row.id}>
+                    {column.hasOwnProperty("render")
+                      ? column.render(row)
+                      : row[column.key]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
-export { default as HomeView } from "./Home"
-export { default as SignInView } from "./SignIn"
-export { default as SignUpView } from "./signUp"
+export { default as HomeView } from "./Home";
+export { default as SignInView } from "./SignIn";
+export { default as SignUpView } from "./signUp";
+export { default as MiInformacionView } from "./MiInformacion/miInformacion";
