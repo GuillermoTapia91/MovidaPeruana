f7941b00cb589382fce7655029b0d59775d99cb4
Kishinkidu <erick.revoredo58@gmail.com>
2023-08-02 18:09:36 -0500

    agrege el login  y registro

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomeView, SignInView, SignUpView, MiInformacionView } from "../pages";
-import MainLayout from "../Layouts/mainLayout";
+import { HomeView, SignInView, SignUpView } from "../pages";
+import MainLayout from "../NavBar/mainLayout";
+import NavBarAuth from "../NavBarAuth";
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomeView />} />
-        </Route>
-        <Route path="/signin" element={<SignInView />} />
-        <Route path="/SignUp" element={<SignUpView />} />
-        <Route path="/mi-informacion" element={<MiInformacionView />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route index element={<HomeView />} /> 
+      </Route>
+      <Route path="/SignUp" element={<SignUpView />} />
+      <Route element={<NavBarAuth />}>
+        <Route path="/Principal" element={<SignInView />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
